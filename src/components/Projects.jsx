1bc87e406012fb649befc56b63a58ddad99e7fd2
mishrayush15 <ayushmishra780193@gmail.com>
2024-10-13 05:10:42 +0530

    add online status to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,16 +8,24 @@ const Projects = () => {
             <div className="projects-page grid sm:grid-cols-3 grid-cols-1 sm:gap-20 gap-10">
                 {projectsList.map((project, index) => {
                     return (
-                        <div
-                            key={index}
-                            className="bg-white p-6 sm:w-[300px] sm:h-[300px] w-[250px] h-[250px] hover:shadow-xl transition-transform duration-300 transform hover:scale-105 flex flex-col items-center cursor-pointer"
-                        >
-                            <img
-                                src={project.image}
-                                alt={project.title}
-                                className="object-cover w-full h-full transition duration-300 ease-in-out transform sm:grayscale hover:grayscale-0"
-                            />
-                            <h3 className="text-xl text-black font-semibold mb-4 text-center">{project.title}</h3>
+                        <div key={index} className="relative flex flex-col items-center">
+                            
+                            {project.working && (
+                                <div className="absolute top-[-20px] left-0 flex items-center space-x-2">
+                                    <svg className="w-4 h-4 text-green-500 animate-pulse" viewBox="0 0 20 20" fill="currentColor">
+                                        <circle cx="10" cy="10" r="8" />
+                                    </svg>
+                                    <span className="text-xs text-green-500">currently working</span>
+                                </div>
+                            )}
+                            <div className="bg-white p-6 sm:w-[300px] sm:h-[300px] w-[250px] h-[250px] hover:shadow-xl transition-transform duration-300 transform hover:scale-105 flex flex-col items-center cursor-pointer">
+                                <img
+                                    src={project.image}
+                                    alt={project.title}
+                                    className="object-cover w-full h-full transition duration-300 ease-in-out transform sm:grayscale hover:grayscale-0"
+                                />
+                                <h3 className="text-xl text-black font-semibold mb-4 text-center">{project.title}</h3>
+                            </div>
                         </div>
                     )
                 })}
--- src/data/ProjectsList.js
+++ src/data/ProjectsList.js
@@ -2,27 +2,32 @@ export const projectsList = [
     {
         title: "Mit-Connect",
         link: "xyz.com",
-        image: "images/projects/mitconnect.png"
+        image: "images/projects/mitconnect.png",
+        working: true
     },
     {
         title: "NoteIt",
         link: "xyz.com",
-        image: "images/projects/noteit.png"
+        image: "images/projects/noteit.png",
+        working: false
     },
     {
         title: "SkySync",
         link: "xyz.com",
-        image: "images/projects/weather.png"
+        image: "images/projects/weather.png",
+        working: false
     },
     {
         title: "Beep Boop",
         link: "xyz.com",
-        image: "images/projects/beepboop.png"
+        image: "images/projects/beepboop.png",
+        working: false
     },
     {
         title: "VirtualR",
         link: "xyz.com",
-        image: "images/projects/virtualr.png"
+        image: "images/projects/virtualr.png",
+        working: false
     },
 ]
 
